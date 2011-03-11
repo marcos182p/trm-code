569a68b51c51032765ea0d0710cb6bcae77be1bc
marcos182p@gmail.com <marcos182p@gmail.com@b57e331e-1f9c-e8ff-0cf7-0d9ef8106651>
2011-03-11 02:11:24 +0000

    [trm-compiler]continuanda a implementação do reconhecedor do automato.

--- trunk/trm-compiler/src/rtm/lexical/Recognizer.java
+++ trunk/trm-compiler/src/rtm/lexical/Recognizer.java
@@ -1,5 +1,8 @@
 package rtm.lexical;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Classe que verifica se uma fita é reconhecida por um automato.
  */
@@ -10,18 +13,26 @@ public class Recognizer {
     private char[] tape;
     
     private State currentState;
-    private State lastState;
+
+    private List<Token> tokens;
     
     private boolean recognized;
 
     public Recognizer(Automaton automaton, char[] tape) {
 
         this.automaton = automaton;
-        
-        this.lastState = null;
-        this.currentState = automaton.getStartState();
+
+        this.tape = tape;
         
         this.recognized = false;
+
+        this.tokens = new ArrayList<Token>();
+
+        reset();
+    }
+
+    private void reset() {
+        this.currentState = automaton.getStartState();
     }
 
     /**
@@ -29,8 +40,30 @@ public class Recognizer {
      */
     public void run() {
 
+        StringBuilder word = new StringBuilder();
+
         for (char c : tape) {
-            transition(c);
+
+            try {
+                
+                transition(c);
+                word.append(c);
+                
+            } catch (TransitionException e) {
+                
+                if(!automaton.getFinalStates().contains(currentState)) {
+                    throw new RuntimeException("Impossivel ler fita", e);
+                }
+
+                Token token = new Token(getTokenClass(currentState));
+                token.setValue(word.toString().trim());
+
+                tokens.add(token);
+
+                word = new StringBuilder();
+                reset();
+
+            }
         }
 
         if(automaton.getFinalStates().contains(currentState)) {
@@ -38,21 +71,57 @@ public class Recognizer {
         }
     }
 
+    //FIXME terminar de implementar esse metodo.
+    private TokenClass getTokenClass(State state) {
+        
+        TokenClass result = TokenClass.TK_UNDEFINED;
+
+        switch(state.getDescription().getWordType()) {
+
+            case WORD:
+                //verificar se a palavra é reservada.  
+                //caso não for, sera um identificador.
+                break;
+            case INTEGER_CTE:
+                result = TokenClass.TK_INTEGER_CTE;
+                break;
+
+        }
+
+        return result;
+    }
+
     public boolean isRecognized() {
         return recognized;
     }
 
-    private void transition(char c) throws RuntimeException {
-        
+    private void transition(char c) {
+
         Transition transition = automaton.findTransition(currentState, c);
 
         if (transition == null) {
-            throw new RuntimeException("Impossivel ir para outro estado.");
+            throw new TransitionException("Transição para esse valor não "
+                    + "encontrada.");
         }
 
-        lastState = currentState;
         currentState = transition.getTarget();
 
+    }
+
+    private class TransitionException extends RuntimeException {
+
+        public TransitionException(Throwable cause) {
+            super(cause);
+        }
+
+        public TransitionException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public TransitionException(String message) {
+
+            super(message);
+        }
     }
 
 }
