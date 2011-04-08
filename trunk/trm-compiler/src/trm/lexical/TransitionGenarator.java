8365983f6afee95ad8f9b6aad1c55fa0d24b27cd
marcos.182p@gmail.com <marcos.182p@gmail.com@b57e331e-1f9c-e8ff-0cf7-0d9ef8106651>
2011-04-08 18:51:46 +0000

    [ut-core]iniciado o gerador de automato.

---
+++ trunk/trm-compiler/src/trm/lexical/TransitionGenarator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trm.lexical;
+
+/**
+ *
+ * @author Marcos Paulo
+ */
+public class TransitionGenarator {
+
+}
