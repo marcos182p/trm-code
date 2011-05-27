64648d224edff1091fcb3c6a46e8c0543efed051
rafaelrocha.ufal@gmail.com <rafaelrocha.ufal@gmail.com@b57e331e-1f9c-e8ff-0cf7-0d9ef8106651>
2011-05-27 12:09:06 +0000

    Começando parte de visualização do tabuleiro

---
+++ trunk/trm-network/src/trm/view/DominoImageLoader.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trm.view;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import trm.core.SquareNumber;
+
+/**
+ *
+ * @author marcos182p
+ */
+public class DominoImageLoader {
+    
+    private static final String PATH = "rsc//domino";
+    private static final String EXTENSION = ".png";
+
+    private DominoImageLoader () {
+    }
+
+    public static Image loadDominoImage(SquareNumber sqrNumber) {
+        String fullPath = PATH + (sqrNumber.ordinal() + 1) + EXTENSION;
+
+        Image dominoImage = new ImageIcon(fullPath).getImage();
+        return dominoImage;
+    }
+}
---
+++ trunk/trm-network/src/trm/view/Drawable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trm.view;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author marcos182p
+ */
+public interface Drawable {
+    public void draw(Graphics g);
+}
