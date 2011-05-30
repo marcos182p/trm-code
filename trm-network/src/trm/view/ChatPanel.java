ce15cacffa7342d7ec802a1081858e3376a327e6
rafaelrocha.ufal <rafaelrocha.ufal@gmail.com>
2011-05-30 14:30:17 +0000

    continuando parte gráfica do chat

--- trm-network/src/trm/view/ChatPanel.java
+++ trm-network/src/trm/view/ChatPanel.java
@@ -2,32 +2,59 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package trm.view;
 
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
 
 /**
  *
  * @author rafanet
  */
-public class ChatPanel extends JPanel{
+public class ChatPanel extends JPanel {
+
     private JLabel label;
     private JTextField chat;
     private JTextArea chatArea;
 
     public ChatPanel() {
-        label = new JLabel("Chat: ");
-        chat = new JTextField(45);
-        chatArea = new JTextArea();
-        add(label);
-        add(chat);
+        this.label = new JLabel("Chat: ");
+        this.chat = new JTextField(45);
+        this.chatArea = new JTextArea();
+        JScrollPane pane = new JScrollPane(chatArea);
+
+        chatArea.setAutoscrolls(true);
+        chatArea.setRows(3);
+
+        chatArea.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        chatArea.setText("Fielasdjaps\nasdasdn\n\nasdasdasd\n\nasdasdasd\n\n");
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.insets = new Insets(5, 5, 5, 5);
+        c.gridx = 0;
+        c.gridy = 0;
+        add(label, c);
+        c.gridx = 1;
+        c.gridy = 0;
+        add(chat, c);
+        c.gridx = 1;
+        c.gridy = 1;
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(pane, c);
+
+
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
     }
 }
