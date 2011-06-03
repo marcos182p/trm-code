4fc8f812c06b2a6499fa56110d4cf51fa92c190f
tm.thyago@gmail.com <tm.thyago@gmail.com@b57e331e-1f9c-e8ff-0cf7-0d9ef8106651>
2011-06-03 15:08:03 +0000

    [trm-compiler]
    - Inclusão da regra IsSymbolRule
    - Adicionado suporta a constantes CHAR e constantes STRING!  
    - Correção de bug no metodo nextState().

--- trunk/trm-compiler/src/trm/lexical/Main.java
+++ trunk/trm-compiler/src/trm/lexical/Main.java
@@ -1,65 +1,84 @@
 package trm.lexical;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import trm.lexical.rules.WhitespaceRule;
 import trm.lexical.rules.ExclusiveRule;
 import trm.lexical.rules.InclusiveRule;
 import trm.lexical.rules.IsDigitRule;
 import trm.lexical.rules.IsLetterRule;
+import trm.lexical.rules.IsSymbolRule;
+import trm.lexical.rules.WhitespaceRule;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        State start = new State("1", new StateDescription());
+
+        State startState = new State("1", new StateDescription());
+
         State idState = new State("2", new StateDescription(WordType.WORD));
 
-        State tempRealState = new State("8", new StateDescription());
+        State fisrtRealState = new State("8", new StateDescription());
         State realState = new State("9", new StateDescription(WordType.REAL_CTE));
-
         State integerState = new State("3", new StateDescription(WordType.INTEGER_CTE));
-        State firstSlash = new State("4", new StateDescription());
-        State firstStar = new State("5", new StateDescription());
-        State secondSlash = new State("6", new StateDescription());
+
+        State firstCharState = new State("10", new StateDescription());
+        State secondCharState = new State("11", new StateDescription());
+        State charState = new State("12", new StateDescription(WordType.CHARACTER_CTE));
+
+        State firstStringState = new State("13", new StateDescription());
+        State stringState = new State("16", new StateDescription(WordType.STRING_CTE));
+
+
+        State firstSlashState = new State("4", new StateDescription());
+        State firstStarState = new State("5", new StateDescription());
+        State secondSlashState = new State("6", new StateDescription());
         State secondStar = new State("7", new StateDescription());
 
-        //ID DIGITO  COMENTARIO (//) COMENTARIO (/**/)
 
-        Automaton automaton = new Automaton(start, idState, integerState, realState);
+        Automaton automaton = new Automaton(startState, idState, integerState, realState, charState, stringState);
 
-        automaton.addTransition(start, start, new WhitespaceRule());
-        automaton.addTransition(start, idState, new IsLetterRule());
-        automaton.addTransition(start, integerState, new IsDigitRule());
-        automaton.addTransition(start, firstSlash, new InclusiveRule('/'));
+        automaton.addTransition(startState, startState, new WhitespaceRule());
+        automaton.addTransition(startState, firstCharState, new InclusiveRule('\''));
+        automaton.addTransition(startState, firstStringState, new InclusiveRule('"'));
+        automaton.addTransition(startState, idState, new IsLetterRule());
+        automaton.addTransition(startState, integerState, new IsDigitRule());
+        automaton.addTransition(startState, firstSlashState, new InclusiveRule('/'));
+
+
+        automaton.addTransition(firstCharState, secondCharState, new IsDigitRule());
+        automaton.addTransition(firstCharState, secondCharState, new IsLetterRule());
+        automaton.addTransition(firstCharState, secondCharState, new IsSymbolRule());
+        automaton.addTransition(firstCharState, secondCharState, new WhitespaceRule());
+
+        automaton.addTransition(secondCharState, charState, new InclusiveRule('\''));
+
+        automaton.addTransition(firstStringState, firstStringState, new IsDigitRule());
+        automaton.addTransition(firstStringState, firstStringState, new IsLetterRule());
+        automaton.addTransition(firstStringState, firstStringState, new IsSymbolRule());
+        automaton.addTransition(firstStringState, firstStringState, new WhitespaceRule());
+
+        automaton.addTransition(firstStringState, stringState, new InclusiveRule('"'));
 
         automaton.addTransition(idState, idState, new IsDigitRule());
         automaton.addTransition(idState, idState, new IsLetterRule());
         automaton.addTransition(idState, idState, new InclusiveRule('_'));
 
         automaton.addTransition(integerState, integerState, new IsDigitRule());
+        automaton.addTransition(integerState, fisrtRealState, new InclusiveRule('.'));
 
-
-        automaton.addTransition(integerState, tempRealState, new InclusiveRule('.'));
-        automaton.addTransition(tempRealState, realState, new IsDigitRule());
+        automaton.addTransition(fisrtRealState, realState, new IsDigitRule());
         automaton.addTransition(realState, realState, new IsDigitRule());
 
+        automaton.addTransition(firstSlashState, secondSlashState, new InclusiveRule('/'));
+        automaton.addTransition(firstSlashState, firstStarState, new InclusiveRule('*'));
 
-        automaton.addTransition(firstSlash, secondSlash, new InclusiveRule('/'));
-        automaton.addTransition(firstSlash, firstStar, new InclusiveRule('*'));
+        automaton.addTransition(secondSlashState, secondSlashState, new ExclusiveRule('\n'));
+        automaton.addTransition(secondSlashState, startState, new InclusiveRule('\n'));
 
-        automaton.addTransition(secondSlash, secondSlash, new ExclusiveRule('\n'));
-        automaton.addTransition(secondSlash, start, new InclusiveRule('\n'));
-
-        automaton.addTransition(firstStar, firstStar, new ExclusiveRule('*'));
-        automaton.addTransition(firstStar, secondStar, new InclusiveRule('*'));
+        automaton.addTransition(firstStarState, firstStarState, new ExclusiveRule('*'));
+        automaton.addTransition(firstStarState, secondStar, new InclusiveRule('*'));
         automaton.addTransition(secondStar, secondStar, new InclusiveRule('*'));
-        automaton.addTransition(secondStar, firstStar, new ExclusiveRule('*', '/'));
-        automaton.addTransition(secondStar, start, new InclusiveRule('/'));
-
-
-
+        automaton.addTransition(secondStar, firstStarState, new ExclusiveRule('*', '/'));
+        automaton.addTransition(secondStar, startState, new InclusiveRule('/'));
 
 
 //        String texto = "";
@@ -74,7 +93,7 @@ public class Main {
 //        }
         //String arquivo = " false true  1.1 d123456 1212d /***aqui também*/ enquanto 3 //se 22\n   3hhg /***Isto é um comentário e deve ser ignorado*/ asdasds 123asd blá ";
 
-        String arquivo = "real integer character boolean void  d d1 d_ d1_  d_1 124 45 4  4.4 true false";
+        String arquivo = " \"sou uma stri\" 'a'";
         Recognizer recognizer = new Recognizer(automaton, arquivo.toCharArray());
 
         recognizer.run();
