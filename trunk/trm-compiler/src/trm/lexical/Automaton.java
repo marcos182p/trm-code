e53fc9f956771b48bf1629618cbe888bd95a2375
tm.thyago@gmail.com <tm.thyago@gmail.com@b57e331e-1f9c-e8ff-0cf7-0d9ef8106651>
2011-06-04 02:32:58 +0000

    [trm-compiler] Criacao da classe CreateAutomaton para retirar do Main (ver como pode melhorar)

--- trunk/trm-compiler/src/trm/lexical/Automaton.java
+++ trunk/trm-compiler/src/trm/lexical/Automaton.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import javax.swing.JOptionPane;
 import trm.lexical.rules.Rule;
 
 public class Automaton {
