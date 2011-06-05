b49e218595f8fa6288ae54a11f5f19b314e59fba
tm.thyago@gmail.com <tm.thyago@gmail.com@b57e331e-1f9c-e8ff-0cf7-0d9ef8106651>
2011-06-05 12:02:01 +0000

    [trm-compiler] Inclusão do JavaDoc na Classe ExclusiveRule

--- trunk/trm-compiler/src/trm/lexical/rules/ExclusiveRule.java
+++ trunk/trm-compiler/src/trm/lexical/rules/ExclusiveRule.java
@@ -1,17 +1,34 @@
 package trm.lexical.rules;
 
-public class ExclusiveRule implements Rule {
+/** Classe para execução de regras exclusivas. Implementa a interface IRule
+ * @author TRM
+ * @version 0.99
+ */
+public class ExclusiveRule implements IRule {
 
     private char[] excludedChars;
 
+    /** Construtor da Classe ExclusiveRule
+     * @param  excludedChars char[] - Caracterer não permitidos
+     */
     public ExclusiveRule(char... excludedChars) {
         this.excludedChars = excludedChars;
     }
 
-    public char[] getTransitionChars() {
+    /** Método que retorna os caracteres não permitidos
+     * @return char[] - Array com os caracteres não permitidos
+     */
+    public char[] getExcludedChars() {
         return excludedChars;
     }
 
+    /** Método de avaliação da regra exclusiva:
+     * caso o elemento c (char) pertença a lista
+     * de não permitidos, retorna false. Caso contrario
+     * retorna true.
+     * @param  c char - Caracterer a ser avaliado
+     * @return boolean - Resultado da avaliação
+     */
     @Override
     public boolean evaluate(char c) {
         for (int i = 0; i < excludedChars.length; i++) {
