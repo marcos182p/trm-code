56d5cf9ef655c2b9a408bf5fccc51b297b6c27c5
tm.thyago <tm.thyago@gmail.com>
2011-06-05 12:20:38 +0000

    [trm-compiler] Inclusao do JavaDoc em todas as classes/interfaces do pacote trm.lexical.rules

--- trm-compiler/src/trm/lexical/rules/IsDigitRule.java
+++ trm-compiler/src/trm/lexical/rules/IsDigitRule.java
@@ -1,8 +1,17 @@
 package trm.lexical.rules;
 
-
+/** Classe para execução de regras de verificação de digitos. Implementa a interface IRule
+ * @author TRM
+ * @version 0.99
+ */
 public class IsDigitRule implements IRule {
 
+    /** Método de avaliação da regra de digito:
+     * caso o elemento c (char) seja um digito,
+     * retorna true. Caso contrario retorna false.
+     * @param  transitionChar char - Caracterer a ser avaliado
+     * @return boolean - Resultado da avaliação
+     */
     @Override
     public boolean evaluate(char transitionChar) {
         return Character.isDigit(transitionChar);
