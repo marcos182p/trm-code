5076d0a9c17cf981c5352e182ac5b6aa5e00eb78
Marcos Paulo <marcos182p@users.noreply.github.com>
2011-06-18 22:35:02 +0000

    [trm-compiler]começando a implementação da analise da definição de função

--- trm-compiler/src/trm/syntactic/Instruction.java
+++ trm-compiler/src/trm/syntactic/Instruction.java
@@ -1,12 +1,61 @@
 package trm.syntactic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import trm.lexical.Token;
+
 /**
  *
  */
 public class Instruction {
     
     public enum InstructionType {
-        DECLARATION, ATTRIBUTION, FOR, WHILE, FUNCTION
-}
+
+        DECLARATION, ATTRIBUTION, FOR, WHILE, FUNCTION, FUNCTION_CALL, IF, ELSE
+    }
     
+    private List<Token> tokens;
+    
+    private InstructionType type;
+    
+    private int start;
+    private int end;
+
+    public Instruction(InstructionType type, List<Token> tokens) {
+        this.tokens = tokens;
+        this.type = type;
+    }
+
+    public List<Token> getTokens() {
+        return Collections.unmodifiableList(tokens);
+    }
+    
+    public InstructionType getType() {
+        return type;
+    }
+    
+    public Token lastToken() {
+        return tokens.get(tokens.size() - 1);
+    }
+    
+    public void addToken(Token token) {
+        tokens.add(token);
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+    
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
 }
---
+++ trm-compiler/src/trm/syntactic/SyntacticAnalyserImpl.java
@@ -0,0 +1,177 @@
+package trm.syntactic;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import trm.lexical.ILexical;
+import trm.lexical.Token;
+import trm.lexical.TokenClass;
+import trm.syntactic.Instruction.InstructionType;
+
+/**
+ *
+ */
+public class SyntacticAnalyserImpl implements SyntacticAnalyser {
+    
+    private Set<InstructionType> blocksType;
+    
+    private List<Instruction> instructions;
+    /**
+     * usado para registrar os blocos de código, sabe onde eles começão e 
+     * terminam
+     */
+    private Stack<Instruction> blocks;
+    
+    public SyntacticAnalyserImpl() {
+        instructions = new ArrayList<Instruction>();
+        
+        blocks = new Stack<Instruction>();
+        blocksType = new HashSet<InstructionType>() {{
+            add(InstructionType.FOR);
+            add(InstructionType.FUNCTION);
+            add(InstructionType.WHILE);
+            add(InstructionType.IF);
+            add(InstructionType.ELSE);
+        }};
+    }
+    
+    public void parse(ILexical lexical) {
+        //primeira chamada de tem que ser a declaração de uma função
+        Token token = null;
+        while ((token = lexical.nextToken()) != null) {
+            switch (token.getTokenClass()) {
+                
+                case TK_ID:
+                    Instruction functionInstruction = 
+                            new FuncitionAnalayser(lexical).analyze(token);
+                    
+                    instructions.add(functionInstruction);
+                    pushBlock();
+                    
+                    instruction(lexical);
+                    break;
+                default:
+                    throw new RuntimeException("");
+            }
+        }
+        
+    }
+    
+    private void pushBlock() {
+        
+        int start = instructions.size() - 1;
+        
+        Instruction instruction = instructions.get(start);
+        if (!blocksType.contains(instruction.getType())) {
+            throw new RuntimeException("essa instrução não é de bloco");
+        }
+        
+        instruction.setStart(start);
+        
+        blocks.push(instruction);
+    }
+    
+    /**
+     * metodo que marca o fim de um bloco de código
+     */
+    private void popBlock() {
+        
+        //ultima instrução do bloco
+        int end = instructions.size() - 1;
+        
+        //instrução do inicio do bloco
+        Instruction startBlock = blocks.pop();
+        
+        startBlock.setEnd(end);
+        
+    }
+    
+    private void saveInstruction(Instruction instruction) {
+        instructions.add(instruction);
+        if (blocksType.contains(instruction.getType())) {
+            pushBlock();
+        }
+
+    }
+    
+    private void instruction(ILexical lexical) {
+        Token token = lexical.nextToken();
+        
+        here : while ((token = lexical.nextToken()) != null) {
+            switch (token.getTokenClass()) {
+                case TK_ID:
+                    saveInstruction(
+                            new InstructionAnalyserImpl(lexical).analyze(token));
+                    break;
+                case TK_FOR:
+                    //analisar se o 'for' é valido, marcar seu inicio
+                    saveInstruction(null);
+                    break;
+                case TK_IF:
+                    //analisar se o 'if' é valido, marcar seu inicio
+                    saveInstruction(null);
+                case TK_ELSE:
+                    //analisar se o 'else' é valido, marcar seu inicio
+                    saveInstruction(null);
+                    break;
+                case TK_CLOSE_CURLY_BRACKET:
+                    //verificar a quem ele pertecence
+                    popBlock();
+                    break;
+                default:
+                    throw new RuntimeException("");
+            }
+        }
+        //fazer analise do codigo dentro da função
+        
+    }
+    public static void main(String[] args) {
+        here :while (true) {
+            int i = 1;
+            switch (i)  {
+                case 1:
+                    break here;
+                        
+            }
+        }
+    }
+    
+    
+    
+    
+    /**
+     * analisa o protipo de uma função
+     */
+    private class FuncitionAnalayser extends CommandAnalyser {
+
+        public FuncitionAnalayser(ILexical lexical) {
+            super(TokenClass.TK_ID, lexical);
+        }
+
+        @Override
+        protected InstructionType doAnalysis(Token token) {
+            
+            switch (nextToken().getTokenClass()) {
+            case TK_OPEN_PARENTHESES:
+                functionParameters();
+                break;
+            default:
+                erro();
+        }
+            
+            return InstructionType.FUNCTION;
+        }
+        
+        private void functionParameters() {
+        switch (nextToken().getTokenClass()) {
+            case TK_CLOSE_PARENTHESES:
+                
+                break;
+        }
+    }
+    }
+
+    
+}
