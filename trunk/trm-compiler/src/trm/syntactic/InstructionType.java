d945d03fa1d7d047271a4302addca927639f8cc8
tm.thyago@gmail.com <tm.thyago@gmail.com@b57e331e-1f9c-e8ff-0cf7-0d9ef8106651>
2011-06-23 17:28:29 +0000

    [trm-compiler] Separação da enumeração InstructionType

---
+++ trunk/trm-compiler/src/trm/syntactic/InstructionType.java
@@ -0,0 +1,49 @@
+package trm.syntactic;
+
+/** Enumeração InstructionType - Define os tipos de instruções possíveis
+ * @author TRM
+ * @version 0.99
+ */
+public enum InstructionType {
+
+    /*
+     * Tipo declaração
+     */
+    DECLARATION,
+    /*
+     * Tipo atribuição
+     */
+    ATTRIBUTION,
+    /*
+     * Tipo for
+     */
+    FOR,
+    /*
+     * Tipo while
+     */
+    WHILE,
+    /*
+     * Tipo função
+     */
+    FUNCTION,
+    /*
+     * Tipo chamada de função
+     */
+    FUNCTION_CALL,
+    /*
+     * Tipo if
+     */
+    IF,
+    /*
+     * Tipo else
+     */
+    ELSE,
+    /*
+     * Tipo fim de bloco
+     */
+    END_BLOCK,
+    /*
+     * Tipo retorno
+     */
+    RETURN
+}
